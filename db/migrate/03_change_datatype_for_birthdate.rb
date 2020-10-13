e32b234dcf66e76d5fc81afc789423120f88df63
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-13 04:37:11 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change
-    change_column(student, birthdate, datetime)
+    change_column(:student, :birthdate, :datetime)
   end
 
 end
